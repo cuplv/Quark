d87c2b7d751b77c5fcfee2dddf2255bb66c90770
jprahladan <prashvirgoan@gmail.com>
2021-11-18 10:13:57 -0700

    modify docker installer

--- src/data/ubuntu18setup.sh
+++ src/data/ubuntu18setup.sh
@@ -5,13 +5,15 @@ sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -sc) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-
+sudo groupadd docker
 sudo usermod -aG docker $USER
+newgrp docker 
+# sudo reboot
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
